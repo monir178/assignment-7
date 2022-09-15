d712cff0866e976b0bc6625e1c292116ce6957ff
monir178 <monir.mzs17@gmail.com>
2022-09-15 21:41:10 +0600

    countdown problem fixed

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ const start = () => {
     if (count == 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
+      countdownOverlay.style.display = "none"; //fixed countdown stuck on display
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
